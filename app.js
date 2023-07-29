62ff9ce8c9e9fe56839b21903892f4a3808fdf49
Prayash Mishra <mishraprayash11@gmail.com>
2023-07-29 15:36:08 +0545

    Little Refactorization

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
 require('dotenv').config()
 require('express-async-errors');
-const express=require('express')
-const app=express()
+const express = require('express')
+const app = express()
+const connectDb = require('./db/connect')
 
-const authRoute=require('./routes/auth')
-const campaignRoute=require('./routes/campaign')
+const authRoute = require('./routes/auth')
+const campaignRoute = require('./routes/campaign')
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-const cors=require('cors')
-app.use(express.json())
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 
-const connectDb = require('./db/connect')
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use('/api/v1/auth',authRoute)
-app.use('/api/v1/campaign',campaignRoute)
-app.get('/',(req,res)=>{
+
+
+// routes
+app.use('/api/v1/auth', authRoute)
+app.use('/api/v1/campaign', campaignRoute)
+
+app.get('/', (req, res) => {
     res.send('bibektimilsina')
 })
+
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port=process.env.PORT||3000;
-const start=async()=>{
-try {
-   await connectDb(process.env.MONGO_URL)
-   app.listen(port,(req,res)=>{
-    console.log("listening on port",port);
-})
-} catch (error) {
-    console.log(error);
-    
+const PORT = process.env.PORT || 3000;
+const startServer = async () => {
+    try {
+        await connectDb(process.env.MONGO_URL_LOCAL)
+        console.log(`DB Connection Succesfull. Host:- ${mongoose.connection.host}`)
+        app.listen(PORT, (req, res) => {
+            console.log(`Server listening on port ${PORT}.....`);
+        })
+    } catch (error) {
+        console.log(`Error occured:- ${error}`);
+    }
 }
-}
-start()
+startServer()
