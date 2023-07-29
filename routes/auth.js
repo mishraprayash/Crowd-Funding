1edbfbd855b14ba75830cc4b184c8f29acb66416
Prayash Mishra <mishraprayash11@gmail.com>
2023-07-29 16:40:24 +0545

    created endpoint route

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
-const express=require('express')
-const router=express.Router()
-const {register,login,forgotPassword,resetPassword}=require('../controllers/auth')
+const express = require('express')
+const router = express.Router()
+const { register, login, forgotPassword, resetPassword, verifyEmail } = require('../controllers/auth')
 
 router.route('/register').post(register)
 router.route('/login').post(login)
 router.route('/forgotpassword').post(forgotPassword)
 router.route('/resetpassword').post(resetPassword)
-module.exports=router;
+
+router.route('/verify/:id/:token').get(verifyEmail)
+
+module.exports = router;
