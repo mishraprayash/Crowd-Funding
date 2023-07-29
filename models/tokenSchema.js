98c91be65b3e0a495824200c0d00ffb31a388cfb
Prayash Mishra <mishraprayash11@gmail.com>
2023-07-29 16:39:05 +0545

    created new token schema

---
+++ models/tokenSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const tokenSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    token: {
+        type: String,
+        required: true
+    },
+    expiresAt: {
+        type: Date,
+        default: function () {
+            return new Date(Date.now() + 10000)
+        }
+    }
+})
+
+const Token = mongoose.model('token', tokenSchema)
+
+module.exports = Token
